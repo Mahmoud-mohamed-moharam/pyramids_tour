cb45ae0dcf423e59f2b8a9757202128515845d20
clear <mahmoud.m.moharam@gmail.com>
2022-01-03 05:53:50 +0200

    just testing the comand

---
+++ app.js
@@ -0,0 +1,31 @@
+//test again for more detaies
+document.getElementById("in").placeholder="enter name";
+document.getElementById("bt").value="submit";
+window.btn1.innerHTML="tested";
+document.getElementById("div1")
+function sayhello() {
+   let name_1 =window.in.value
+    document.getElementById("in").value="hello"+" "+name_1;
+}
+//return function
+function fun_2() {
+    return "malek _ id::";
+}
+window.div1.style.backgroundColor="gray";
+window.div1.style.width="680px";
+window.div1.style.height="500px";
+window.div12.style.backgroundColor="yellow";
+window.div12.style.width="450px";
+window.div12.style.height="300px";  
+
+function fun_3(div){
+    div.style.backgroundColor="#111100";
+    div.style.height="100px";
+    div.style.width="100px";
+}
+
+function getxy(event_1) {
+let x =event_1.clientX;
+let y = event_1.clientY;
+window.h11.innerHTML=" X: " + x + " Y: " + y ;
+}
